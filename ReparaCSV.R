6802768632496378faa5a16e9af1ac6e583dae39
Thales Lima <medvet21@gmail.com>
2020-09-25 11:20:27 -0300

    Atualização de arquivo

--- ReparaCSV.R
+++ ReparaCSV.R
@@ -4,8 +4,15 @@
 #
 ################################################################################
 
+
+rm(list=ls())
+# Não deixar o R converter automaticamente caracter em fator
+options(stringsAsFactors=F) 
+
+# 1. ARQUIVOS DE COTAÇÕES DA SCOT CONSULTORIA ##################################
+
 # Indicando diretório onde estão os arquivos
-setwd('C:/Users/Thales/Dropbox/Public/AgriBIsiness/DadosMF/2020')
+setwd('C:/Users/Thales/Dropbox/Repositories/DataCattle')
 
 # listando todos os arquivos com extensão .csv
 input <- dir(pattern='.csv')
@@ -22,3 +29,27 @@ for (i in 1:L){
               row.names = F, col.names = F, sep=";")
   cat(input[i],'\n')
 }
+
+
+# 2. ARQUIVOS DO INDICADOR BOI GORDO CEPEA/B3 ##################################
+
+# Indicando diretório onde estão os arquivos
+setwd('C:/Users/Thales/Dropbox/Repositories/DataCattle')
+
+# Lendo arquivo e visualizando dados
+dados <- read.csv("CEPEA.csv", header = FALSE, col.names = c("Data", "Valor"), 
+                  sep = ",", dec = ".")
+head(dados)
+str(dados)
+
+# Corrigindo formatação
+dados$Data  <- as.Date(dados$Data, format = " %d/%m/%Y ")
+dados$Valor <- as.character(dados$Valor)
+
+dados$Valor <- str_replace(dados$Valor, "\\.", "\\,")
+head(dados)
+str(dados)
+
+# Salvando arquivo .csv
+write.table(dados, file = "IndBG_CEPEA.csv", quote = F, row.names = F, 
+            col.names = T, sep=";")
