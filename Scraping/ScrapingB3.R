725a256c599d4c5cc5cdd170edcf289b2710ee43
Thales de Lima Silva <medvet21@gmail.com>
2021-03-07 15:30:04 -0300

    Update ScrapingB3.R

--- Scraping/ScrapingB3.R
+++ Scraping/ScrapingB3.R
@@ -1,12 +1,12 @@
 ################################################################################## 
 #
-# WEB SCRAPING para ajustes do pregão de Boi Gordo na B3
+# WEB SCRAPING para ajustes do pregao de Boi Gordo na B3
 #
-# V.1 (Jan./2021)                                     Dev.: Thales de Lima Silva  
+# V.1.1 (Mar/2021)                                     Dev.: Thales de Lima Silva  
 #
 ##################################################################################
 
-# Obs: Caso tenha problemas com a acentuação, consulte este link:
+# Obs: Caso tenha problemas com a acentuacao, consulte este link:
 # https://support.rstudio.com/hc/en-us/articles/200532197-Character-Encoding
 
 rm(list=ls())
@@ -14,21 +14,20 @@ setwd("C:/Users/Thales/Dropbox/Repositories/DataCattle/")
 
 # Carregando pacotes
 library(rvest) 
-#library(stringr) 
 library(dplyr) 
-
+library(zoo)
 
 # Inserindo a URL alvo - Austes Derivativos - Pregao
 url <- 'http://www2.bmf.com.br/pages/portal/bmfbovespa/lumis/lum-ajustes-do-pregao-ptBR.asp'
 
-# Lendo código HTML da URL indicada
+# Lendo codigo HTML da URL indicada
 site <- read_html(url)
 
 # Escolhendo os elementos HTML para coletar - resultado em HTML
 info_Ajuste_HTML <- html_nodes(site,'table')
 date_HTML <- html_nodes(site, 'div.large-9.medium-4.columns')
 
-# Melhorando a visualização e captura dos objetos
+# Melhorando a visualizacao e captura dos objetos
 lista_tabela <- site %>%
   html_nodes("table") %>%
   html_table(fill = TRUE)
@@ -36,19 +35,31 @@ lista_tabela <- site %>%
 date_txt <- html_text(date_HTML, trim = TRUE)
 date_txt <- strsplit(date_txt, "\r\n")
 
-# Atribuições do preço de ajuste e data
+# Atribuicoes do pregao de ajuste e data
 ajuste <- lista_tabela[[1]]
-bgi <- ajuste[28:38,]
+ajuste$Mercadoria[ajuste$Mercadoria == ""] <- NA
+ajuste <- na.locf(ajuste)
+
+bgi <- filter(ajuste, Mercadoria == "BGI   - Boi gordo")
 ajuste_atual <- bgi[1,4]
 
 date_list <- c(strsplit(date_txt[[1]], " "))
 date_ajt <- as.Date(date_list[[1]][3], format = " %d/%m/%Y ")
 
-# Criando data.frame com dados selecionados
+
+## DATA.FRAME COM DADOS DOS CONTRATOS
+ajat <- data.frame(bgi[,c(2,4,5)], row.names = NULL)
+colnames(ajat) <- c("Contrato","Ajuste","Variacao")
+ajat["Data"] <- rep(date_ajt)
+write.table(ajat, "Ajustes.csv", row.names = T, col.names = T,
+            quote = FALSE, sep = ";")
+
+
+## DATA.FRAME COM DADOS DE FECHAMENTO DO AJUSTE MAIS PROXIMO
 df <- data.frame(date_ajt, ajuste_atual)
 df <- df %>% rename(Data = date_ajt) %>% rename(Ajuste = ajuste_atual)
 
-# Lendo arquivo de dados e corrigindo a formataçãoo
+# Lendo arquivo de dados e corrigindo a formatacao
 bgi_close <- read.csv("BGI_Fut.csv", header = T,  sep = ";",
                       col.names = c("Data", "Ajuste"))
 bgi_close1 <- bgi_close %>%
@@ -57,7 +68,7 @@ bgi_close1 <- bgi_close %>%
 
 # Incluindo dados coletados ao data.frame
 lastdate <- max(bgi_close1$Data)
-lastdate
+#lastdate
 
 if (df$Data != lastdate) {
   bgi_close1 <- rbind(bgi_close1, df)
